76192aca5a2165fe839c67ad7c28e97bca3ba472
451629802 <451629802@qq.com>
2017-11-05 22:39:28 +0800

    基本完成了我的页面的交互，后期需要优化，以及完成了判断当前的用户是否是登录的状态

--- pages/myInfo/myInfo.js
+++ pages/myInfo/myInfo.js
@@ -4,30 +4,47 @@ Page({
    * 页面的初始数据
    */
   data: {
-    isShow : false,
+    isShow: getApp().globalData.isShow,
     seedPhone: '',
-    seedPassword: ''
+    seedPassword: '',
+    myLists : [
+      { url: 'https://mhero.pinhui100.com/WebApp/img/mine_production.png', content: '我的作品'},
+      { url: 'https://mhero.pinhui100.com/WebApp/img/mine_dynamic.png', content: '我的动态' },
+      { url: 'https://mhero.pinhui100.com/WebApp/img/mine_account.png', content: '账户' },
+      { url: 'https://mhero.pinhui100.com/WebApp/img/mine_real.png', content: '实名认证' },
+      { url: 'https://mhero.pinhui100.com/WebApp/img/mine_quit.png', content: '退出登录' },
+    ],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this;
+    wx.getStorage({
+      key: 'tokens',
+      success: function(res) {
+        if(res.data.token){
+          that.setData({
+            isShow : !that.data.isShow
+          })
+        }
+      },
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+   
   },
 
   /**
@@ -93,21 +110,62 @@ Page({
     var that = this;
     if (this.data.seedPhone && this.data.seedPassword){
       wx.request({
-        url: 'https://designer.pinhui100.com/api/user/login',
+        url: 'https://designer.pinhui100.com/api/app/user/normal/login',
         data: {
-          designer_phone: this.data.seedPhone,
-          designer_password: this.data.seedPassword
+          username: that.data.seedPhone,
+          password: that.data.seedPassword
         },
         method: 'POST',
         dataType: 'json',
         success: function (res) {
-          console.log(res.data)
+          if (res.data.status_code == 404){
+            wx.showModal({
+              content: res.data.message
+            })
+          } else if (res.data.status_code == 401){
+            wx.showModal({
+              content: res.data.message
+            })
+          } else if (res.data.status_code == 500){
+            wx.showModal({
+              content: res.data.message
+            })
+          } else{
+            // 将后台传来的登录后的token储存在本地
+            wx.setStorage({
+              key: 'tokens',
+              data: res.data,
+              success : function(){
+                wx.showModal({
+                  content: '登陆成功',
+                })
+                that.setData({
+                  isShow: !that.data.isShow
+                })
+              }
+            })
+          }
         },
       })
     }else{
       wx.showModal({
         content : '手机号码或者密码不能为空'
       })
+    }
+  },
+  // 这是当客户点击退出的时候我们需要将本地的tokens的值清除
+  listEvent(e){
+    if (e.currentTarget.id == 4){
+      try {
+        wx.clearStorageSync()
+        this.setData({
+          isShow : false,
+          seedPhone: '',
+          seedPassword: '',
+        })
+      } catch (e) {
+        console.log('你好清除数据失败，错误信息 ：' + e)
+      }
     }
   },
 })
