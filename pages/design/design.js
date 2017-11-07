c5e5ace4da9ade08d8c02446c17ee99072ccdfb9
451629802 <451629802@qq.com>
2017-11-07 17:41:13 +0800

    设计圈的朋友列表的样式以及 在请求头header中带入token

--- pages/design/design.js
+++ pages/design/design.js
@@ -4,14 +4,49 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    isShow: getApp().globalData.isShow,
+    token : '',
+    transmit: '',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    let that = this;
+    wx.getStorage({
+      key: 'tokens',
+      success: function (res) {
+        if (res.data.token) {
+          that.setData({
+            isShow: !that.data.isShow,
+            token: res.data.token,
+          })
+          wx.request({
+            url: 'https://designer.pinhui100.com/api/app/mood/showlist',
+            method: 'POST',
+            data: {
+              method: 2,
+              page_no: 1,
+              page_size: 5,
+            },
+            header: {
+              "Authorization": "Bearer " + that.data.token,
+              "Content-Type": "application/x-www-form-urlencoded"
+            },
+            dataType: 'json',
+            success: function (res) {
+              console.log(res.data)
+              if(res.data.errno == 0){
+                  
+              }else{
+                console.log(res.data.data.msg)
+              }
+            }
+          })
+        }
+      },
+    })
   },
 
   /**
